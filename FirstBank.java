05a9169e28d30baa9ccecf1eff989cc4cf300d14
aqaisar3 <97372395+ayeshaq2@users.noreply.github.com>
2022-12-29 20:28:09 -0500

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,15 @@
+public class FirstBank {
+    public static void main(String [] bank){
+
+
+        Account tim= new Account("Time Statler", "A00001");
+       // tim.showMenu();
+
+        Account ayesha = new Account("Ayesha Qaisar", "AQ002");
+        ayesha.showMenu();
+
+
+
+
+    }
+}
